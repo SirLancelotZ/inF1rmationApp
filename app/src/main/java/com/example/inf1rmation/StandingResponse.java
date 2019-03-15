b32cceab023c99d7f581ba3fe9dc4e1e89d61aa2
Lance <ngtlancezhu@gmail.com>
2019-03-15 09:40:35 -0700

    revision#2

---
+++ app/src/main/java/com/example/inf1rmation/StandingResponse.java
@@ -0,0 +1,22 @@
+package com.example.inf1rmation;
+
+import java.util.List;
+
+public class StandingResponse {
+
+    public List<StandingResponse> getStandingsLists() {
+        return StandingsLists;
+    }
+
+    public void setStandingsLists(List<StandingResponse> standingsLists) {
+        StandingsLists = standingsLists;
+    }
+
+    private List<StandingResponse> StandingsLists;
+
+
+
+
+
+
+    }
